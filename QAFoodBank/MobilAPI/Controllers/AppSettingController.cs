89f688b9b7d76940c791e548ba72d3af1c057984
Suresh Pokkuluri <psureshmsc@hotmail.com>
2017-10-08 13:20:43 -0700

    AppSettings End Point

---
+++ QAFoodBank/MobilAPI/Controllers/AppSettingController.cs
@@ -0,0 +1,20 @@
+﻿using QAFoodBank.MobileAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QAFoodBank.MobileAPI.Controllers
+{
+    public class AppSettingController : ApiController
+    {
+        private QAFB_DBEntities db = new QAFB_DBEntities();
+
+        public IEnumerable<AppSettng> Get()
+        {
+            return db.AppSettngs.ToList();
+        }
+    }
+}
