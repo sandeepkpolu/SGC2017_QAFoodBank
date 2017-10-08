b97a5761b5fd08af0d468163b205e7b5ce7063c4
Evan Dong <rover@Evans-MacBook-Pro.local>
2017-10-07 17:30:45 -0700

    this is iOS notification

--- QAFoodBank/QAFoodBank/App.xaml.cs
+++ QAFoodBank/QAFoodBank/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Push;
 using QAFoodBank.Helpers;
 using System;
 
@@ -17,13 +18,38 @@ namespace QAFoodBank
         {
             InitializeComponent();
 
+			Push.PushNotificationReceived += (sender, e) => {
+
+                string summary = String.Empty;
+
+				if (e.CustomData != null)
+				{
+					summary = "\n\tCustom data:\n";
+					foreach (var key in e.CustomData.Keys)
+					{
+						summary += $"\t\t{key} : {e.CustomData[key]}\n";
+					}
+
+                    System.Diagnostics.Debug.WriteLine(summary);
+				}
+
+                if (MainPage != null)
+                {
+                    Xamarin.Forms.Device.BeginInvokeOnMainThread(()=>
+                    {
+                        MainPage.DisplayAlert(e.Title ?? String.Empty, e.Message + summary, "OK");
+                    });
+                }
+			};
+
             MobileCenter.Start($"android={Constants.MobileCenterAndroid};" +
                    $"uwp={Constants.MobileCenterUWP};" +
                    $"ios={Constants.MobileCenteriOS}",
-                   typeof(Analytics), typeof(Crashes));
+                   typeof(Analytics), typeof(Crashes), typeof(Push));
 
+			Push.SetEnabledAsync(true);
 
-            if (UseMockDataStore)
+			if (UseMockDataStore)
                 DependencyService.Register<MockDataStore>();
             else
                 DependencyService.Register<CloudDataStore>();
