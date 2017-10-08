84f5c74f110ff8e6e85e6fb870171e0543f0d92a
evansong <evansong@yahoo.com>
2017-10-08 11:12:25 -0700

    Adding API Project

---
+++ QAFoodBank/MobilAPI/Controllers/ItemsController.cs
@@ -0,0 +1,47 @@
+﻿using QAFoodBank.MobileAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QAFoodBank.MobileAPI.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private QAFB_DBEntities db = new QAFB_DBEntities();
+        // GET: api/Items
+        public IEnumerable<QAFoodBankItem> Get()
+        {
+            var listOfItems = db.GetListOfItems().ToList();
+            List<QAFoodBankItem> items = new List<QAFoodBankItem>();
+            foreach (var dbItem in listOfItems)
+            {
+                QAFoodBankItem item = new QAFoodBankItem();
+                item.ItemId = dbItem.ItemId;
+                item.ItemName = dbItem.ItemName;
+                item.Description = dbItem.Description;
+                item.Picture = dbItem.Picture;
+                item.CategoryId = (int)dbItem.CategoryId;
+                item.CategoryName = dbItem.CategoryName;
+                item.Urgent = (bool)dbItem.Urgent;
+
+
+                var sources = db.GetItemSources(item.ItemId);
+                var soruceUrls = new List<Vendor>();
+                foreach (var source in sources)
+                {
+                    soruceUrls.Add(new Vendor()
+                    {
+                        Name = source.Name,
+                        Url = source.URL
+                    });
+                }
+                item.SourceUrls = soruceUrls;
+                items.Add(item);
+            }
+            return items;
+        }
+    }
+}
---
+++ QAFoodBank/MobilAPI/Models/QAFoodBankItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QAFoodBank.MobileAPI.Models
+{
+    public class QAFoodBankItem
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool Urgent { get; set; }
+        public List<Vendor> SourceUrls { get; set; }
+    }
+}
